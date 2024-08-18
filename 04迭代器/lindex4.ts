6cd9db6b5ffafe047e7665d8f988e0a5f0a620a4
SouthernPenguin <940583551@qq.com>
2024-08-18 15:12:12 +0800

    添加注释

--- 04迭代器/lindex4.ts
+++ 04迭代器/lindex4.ts
@@ -2,10 +2,10 @@
 
 function* traverse(elemList: Element[]): any {
   for (let elem of elemList) {
-    yield elem;
+    yield elem; // 使用 `yield` 关键字返回当前遍历到的元素。这使得生成器函数在每次调用 `next()` 方法时返回一个值。
     const children = Array.from(elem.children);
     if (children.length) {
-      yield* traverse(children);
+      yield* traverse(children); // yield* 语句用于委托生成器
     }
   }
 }
